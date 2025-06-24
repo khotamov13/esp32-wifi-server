ac8a2bd5ae7724113b438125016058d110253bf0
khotamov13 <jurabekhotamov1234@gmail.com>
2025-06-25 02:57:18 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,35 @@ const char* ssid = "Infix smart 8";
 const char* password = "12345678";
 const IPAddress apIP(192,168,4,1);
 const byte DNS_PORT = 53;
+
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  SPIFFS.begin(true);
+  
+  // SPIFFSni boshlash
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS boshlanmadi!");
+    return;
+  }
+
+  // Wi-Fi AP sozlash
   WiFi.softAPConfig(apIP, apIP, IPAddress(255,255,255,0));
   WiFi.softAP(ssid, password);
+  Serial.println("WiFi AP ochildi");
+
+  // DNS: barcha so‘rovni ESP32'ga yo‘naltirish
   dnsServer.start(DNS_PORT, "*", apIP);
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+
+  // Static fayllarni berish
+  server.serveStatic("/", SPIFFS, "/");
+
+  // Fallback: notFound bo‘lsa ham index.html qaytarilsin
+  server.onNotFound([](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
   server.begin();
 }
 
